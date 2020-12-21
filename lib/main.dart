eb6b16608db4f7a8a7f25427ccb7b0cb2789b52c
Nguyen Tien Dung/Media Platform P /SVMC/Engineer/Samsung Electronics <dung.nt13@samsung.com>
2020-12-21 18:20:55 +0700

    Expaned and Intention Action [51-52]

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,28 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Image(image: AssetImage('images/dice1.png')),
+                )),
+            // SizedBox(
+            //   width: 20,
+            // ),
+            Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Image.asset('images/dice2.png'),
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
